02245eae5cae7bf22c5b583da9ae72b51722fe0f
Phichayut Somjai <phichayut.somjai01@gmail.com>
2025-07-04 02:56:32 +0700

    changed providers' folder location

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import UserProvider from "@/components/ui/providers/UserProvider";
+import UserProvider from "@/components/providers/UserProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
